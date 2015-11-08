08d423144cd42f2de69a9548ed2974a1a4ea719b
Marcin <kielkowskimarcin@prokonto.pl>
2015-11-08 19:26:40 +0100

    Add sattement to help

--- generators/help/SettlementHelp.java
+++ generators/help/SettlementHelp.java
@@ -7,8 +7,29 @@ import javax.swing.JLabel;
  * @author Domek
  */
 public class SettlementHelp implements HelpStrategy {
-    
-    String textT = "tekst", titleT = "Tytuł 3";
+
+    String textT = "<html>"
+            + "<p>"
+            + "<u>Rozliczenia miesięczne</u> - "
+            + "Wyświetla table z<br/> rozliczeniami miesięcznymi.<br/>"
+            + " Mamy możliwość wygenrowania pliku<br/> na postawie tej tabeli. "
+            + "</p>"
+            + "<p>"
+            + "<u>Rozliczenia roczne</u> - "
+            + "Wyświetla table z<br/> rozliczeniami do wybranego dnia.<br/>"
+            + " Mamy możliwość wygenrowania pliku<br/> na postawie tej tabeli. "
+            + "</p>"
+            + "<p>"
+            + "<u>Inwentaryzacja</u> - "
+            + "Wyświetla table z<br/> przedmiotami do wybranego dnia.<br/>"
+            + " Mamy możliwość wygenrowania pliku<br/> na postawie tej tabeli. "
+            + "</p>"
+            + "<p>"
+            + "<u>Raport dzienny</u> - "
+            + "Generuje nam plik z operacjami<br/> jakie zostały przeprowadzone danego dnia."
+            + "</p>"
+            + "</html>";
+    String titleT = "<html><h2>Rozlizenia</h2></html>";
 
     @Override
     public void getText(JLabel title, JLabel text) {
