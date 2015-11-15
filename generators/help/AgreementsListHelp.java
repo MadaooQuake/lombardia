658bc5a2b294591bb258386b10aef302936cecd0
Marcin <kielkowskimarcin@prokonto.pl>
2015-11-15 15:12:27 +0100

    help part 4

---
+++ generators/help/AgreementsListHelp.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lombardia2014.generators.help;
+
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Domek
+ */
+public class AgreementsListHelp implements HelpStrategy {
+
+    @Override
+    public void getText(JLabel title, JLabel text) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
