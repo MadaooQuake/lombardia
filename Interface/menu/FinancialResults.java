25e3187e6b67e554b7949334820189b8e7bbe110
Marcin <kielkowskimarcin@prokonto.pl>
2015-12-16 21:14:15 +0100

    add main frame in financal result

--- Interface/menu/FinancialResults.java
+++ Interface/menu/FinancialResults.java
@@ -1,10 +1,13 @@
 package lombardia2014.Interface.menu;
 
 import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -20,24 +23,32 @@ public class FinancialResults extends MenuElementsList {
     String formname = "Wyniki finanoswe";
     JTable listSettlement = null;
     JScrollPane scrollPane = null;
-    int selectRow = -1, windowWidth = 860, windowHeigth = 500, rowsPerPage = 20;
+    int selectRow = -1, windowWidth = 860, windowHeigth = 600, rowsPerPage = 20;
     String outputFileName = "_be_changed.pdf";
     String[] headers = {"Data", "Dane", "Sprzedaż", "Zwrot pożyczki", "Suma Końcowa"};
+    float[] headersWidth = {1.0f, 7.0f, 3.0f, 3.0f, 3.0f};
     List<Integer> sumMe = new ArrayList();
     List<Integer> translateDateColumn = new ArrayList();
-    float[] headersWidth = {};
     String summaryText = "Podsumowanie";
     int summaryColumnIndex = 2;
     MainDBQuierues DB = new MainDBQuierues();
 
     @Override
     public void generateGui() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       formFrame.setSize(windowWidth, windowHeigth);
+        formFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        formFrame.setResizable(false);
+        mainPanel = new JPanel(new GridBagLayout()); 
+        
+        generatePanels(new GridBagConstraints());
+
+        formFrame.add(mainPanel);
+        formFrame.setVisible(true);
     }
 
     @Override
     public void generatePanels(GridBagConstraints c) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
     
 }
